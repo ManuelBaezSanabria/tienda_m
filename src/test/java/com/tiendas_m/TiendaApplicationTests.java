ea6e6559843c4e9e28c7b5550391d268b53806b6
ManuelBaezSanabria <manuelbsanabria@gmail.com>
2024-05-22 23:16:00 -0600

    Semana 1: Proyecto Incial

---
+++ src/test/java/com/tiendas_m/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendas_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
